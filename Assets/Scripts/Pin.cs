1b755b4241a43bbccef2a5aa61ec76894622ecb4
excitexcite <excitenew@gmail.com>
2021-03-26 03:14:57 +0200

    Add and adjust pin. Add pin move script. Add detection if pin collides with rotator. Implement pin's ability to continue rotatin with rotator if pin collided the rotator

---
+++ Assets/Scripts/Pin.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour
+{
+
+    [SerializeField] private float speed = 20f;
+    [SerializeField] Rigidbody2D rigidbody;
+    private bool isPinned = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!isPinned)
+        {
+            rigidbody.MovePosition(rigidbody.position + Vector2.up * speed * Time.fixedDeltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collisionObj)
+    {
+        if (collisionObj.tag == "Pin")
+        {
+            // TODO GAMEOVER
+        }
+        else if (collisionObj.tag == "Rotator")
+        {
+            isPinned = true;
+            transform.SetParent(collisionObj.transform);
+        }
+    }
+
+}
